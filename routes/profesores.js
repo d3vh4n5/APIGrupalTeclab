ec1b760e1f106a534a96a5ac4395710645b6f4ae
Juan <juanangelbasgall@hotmail.com>
2023-05-28 02:51:01 -0300

    lleve la funcion asíncrona de querys a db.js y lo exporté para simplificar las querys a la db, intenté resumir los métodos get en una funcion para exportar, lo logré pero no pude exportarla, aún así funciona dentro de su módulo, y le agregué un poco de devolución de errores

--- routes/profesores.js
+++ routes/profesores.js
@@ -1,43 +1,48 @@
+// @author : Juan Angel Basgall - https://www.linkedin.com/in/juanangelbasgall/
+
 const express = require('express');
 const router = express.Router()
 // const profesores = require('../fake/profesores');
-const { db_local, db_real } = require('../db');
+const { db_local, db_real, datos} = require('../db');
 
-const datos = async () => {
-	return new Promise((resolve, reject) => {
-		db_local.query('SELECT * FROM profesores', (error, results) => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve(results); // El resolve seria igual al return, pero es para funcion async
-			}
-		});
-	});
-};
+const getStandard = (endpoint, finder = null, fAttr = null) =>{
 
-router.get('/profesores', async (req, res) => {
-	const page = +req.query.page; //tomo las variables despies del ? de la url
-	const pageSize = +req.query.pageSize;
-	let profesores = await datos()
+	router.get(`/${endpoint}`, async (req, res) => {
+		const page = +req.query.page; //tomo las variables despies del ? de la url
+		const pageSize = +req.query.pageSize;
+		let data = await datos(`SELECT * FROM ${endpoint}`)
+		if (data === 'error'){ 
+			return  res.status(404).send('Hubo un error')
+		} else{
 	
-	if (page && pageSize) {
-		const start = (page - 1) * pageSize;
-		const end = start + pageSize;
-		console.log('Ejecutado método get paginado de profesores');
-		res.json(profesores.slice(start, end));
-	} else {
-		//Esta solo la queri porque los metodos conect y end me la hacían crashear
-		console.log('Ejecutado método get de profesores');
-		res.json(profesores)
-	}
-})
+			if (page && pageSize) {
+				const start = (page - 1) * pageSize;
+				const end = start + pageSize;
+				console.log(`Ejecutado método get paginado de ${endpoint}`);
+				res.json(data.slice(start, end));
+			} else {
+				console.log(`Ejecutado método get de ${endpoint}`);
+				res.json(data)
+			}
+		}
+	})
+	
+	router.get(`/${endpoint}${finder}`, async (req,res)=>{
+		let find = req.params.finder
+		let data = await datos(`SELECT * FROM ${endpoint} WHERE ${fAttr}=${find}`)
+		console.log('Se consultó por : ',data);
+		if (data.length === 0 || data === 'error'){ 
+			return  res.status(404).send('Hubo un error, o el recurso no existe')
+		} else{
+			// res.json(profesores.find( p => p.id === + find)) //Busca donde combina el parametro id, con el id de los objetos en profesores
+			res.json(data)
+		}
+	})
+}
 
-router.get('/profesores/:dni', async (req,res)=>{
-	let dni = req.params.dni
-	let profesores = await datos()
-    res.json(profesores.find( p => p.dni === + dni)) //Busca donde combina el parametro id, con el id de los objetos en profesores
-})
+getStandard('profesores', '/:finder', 'dni')
 
 
 
 module.exports = router;
+// module.exports = getStandard;
